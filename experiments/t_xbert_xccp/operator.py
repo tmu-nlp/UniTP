2e54c886f6ef62dfb2fbf323d2be45bee31e7a3b
zchen0420 <feelmailczs@gmail.com>
2022-01-20 19:00:09 +0900

    stable cuda stuffs for paper

---
+++ experiments/t_xbert_xccp/operator.py
@@ -0,0 +1,19 @@
+from experiments.t_lstm_xccp.operator import DiscoMultiOperator
+
+class DiscoMultiOperator_lr(DiscoMultiOperator):
+    def _get_optuna_fn(self, train_params):
+        from utils.train_ops import train, get_optuna_params
+        optuna_params = get_optuna_params(train_params)
+
+        def obj_fn(trial):
+            def spec_update_fn(specs, trial):
+                self._train_materials = None, self._train_materials[1]
+                lr = specs['train']['learning_rate']
+                specs['train']['learning_rate'] = lr = trial.suggest_loguniform('learning_rate', 1e-6, lr)
+                return f'lr={lr:.1e}'
+
+            self._mode_trees = [], [] # force init
+            self.setup_optuna_mode(spec_update_fn, trial)
+
+            return train(optuna_params, self)['key']
+        return obj_fn
